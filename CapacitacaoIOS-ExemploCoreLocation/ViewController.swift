59ca52b0e8faa1da481518b3b564d0aaec5d3625
ALUNO <aluno@ladmac0102.local>
2019-03-22 19:24:52 -0300

    Initial Commit

---
+++ CapacitacaoIOS-ExemploCoreLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapacitacaoIOS-ExemploCoreLocation
+//
+//  Created by ALUNO on 22/03/19.
+//  Copyright © 2019 ALUNO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
